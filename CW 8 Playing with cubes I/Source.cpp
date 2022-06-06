e4545e57ba1d91389c85c04190bb0559d57600be
AliaksandrKryvapust <alexandrkryvopust@gmail.com>
2022-06-06 11:21:54 +0400

    Добавьте файлы проекта.

---
+++ CW 8 Playing with cubes I/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class Cube{
+public:
+	int GetSide() { return Side; }
+	void SetSide(int num) { this->Side = num; }
+private:
+	int Side=0;
+};
+
+
+int main() {
+
+	return 0;
+}
